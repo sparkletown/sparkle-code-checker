4029659e9f68a8a7ff4f68eb8c52aee3126ca5a3
Chris Adams <chris@cadams.com.au>
2020-08-30 10:54:29 -0700

    Initial commit

---
+++ code-checker.sh
@@ -0,0 +1,1 @@
+cd $(dirname $0) && PORT=3000 docker-compose up -d --build
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import fs from "fs";
+
+const CODES_FILE_PATH = "codes.txt";
+const codes = fs.readFileSync(CODES_FILE_PATH).toString().split("\n");
+console.log(`Loaded ${codes.length} codes from ${CODES_FILE_PATH}.`);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/code/:code", (request, response) => {
+  const { code } = request.params;
+  const exists = codes.includes(code);
+  console.log(
+    `Request: code ${code} - ${exists ? "EXISTS" : "DOES NOT EXIST"}`
+  );
+  exists
+    ? response.status(200).send(`${code} OK`)
+    : response.status(404).send(`${code} not found`);
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on ${port}`);
+});
