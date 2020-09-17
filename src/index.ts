04d518c0faa50d58d98ef258dfd8fdd98b473018
Chris Adams <chris@cadams.com.au>
2020-09-17 20:29:04 +0300

    Adapt to Quicket

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,26 @@
 import express from "express";
-import fs from "fs";
-
-const CODES_FILE_PATH = "codes.txt";
-const codes = fs.readFileSync(CODES_FILE_PATH).toString().split("\n");
-console.log(`Loaded ${codes.length} codes from ${CODES_FILE_PATH}.`);
+import axios from "axios";
+import { CHECK_URL, USER_TOKEN, USER_TOKEN_HEADER } from "./secrets";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT ?? 3000;
 
-app.get("/code/:code", (request, response) => {
-  const { code } = request.params;
-  const exists = codes.includes(code);
-  console.log(
-    `Request: code ${code} - ${exists ? "EXISTS" : "DOES NOT EXIST"}`
-  );
-  exists
-    ? response.status(200).send(`${code} OK`)
-    : response.status(404).send(`${code} not found`);
+app.get("/code/:email", async (request, response) => {
+  const { email } = request.params;
+  try {
+    await axios.get(`${CHECK_URL}${email}`, {
+      headers: {
+        [USER_TOKEN_HEADER]: USER_TOKEN,
+      },
+    });
+    response.status(200).send(`${email} OK`);
+  } catch (e) {
+    response
+      .status(404)
+      .send(
+        `hastickets check for ${email} returned ${e.response?.status}: ${e.message}`
+      );
+  }
 });
 
 app.listen(port, () => {
